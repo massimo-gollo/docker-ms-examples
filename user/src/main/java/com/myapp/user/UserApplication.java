642d0dbace4702986abdf04e7ee0dd670882560b
massimogollo <massimo.gollo@hotmail.it>
2022-10-14 16:05:47 +0200

    fix main error command line runner

--- user/src/main/java/com/myapp/user/UserApplication.java
+++ user/src/main/java/com/myapp/user/UserApplication.java
@@ -8,25 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication implements CommandLineRunner {
-
-    @Autowired
-    private UserRepository repository;
-
+public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        // save a couple of customers
-        repository.save(new User("Alice", "Smith"));
-        repository.save(new User("Bob", "Smith"));
-
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-
-    }
 }
