02ded5ed153590720dac4024d3e559748368265b
massimo <massi.gollo@gmail.com>
2022-10-19 17:26:30 +0200

    refactor

--- user/src/main/java/com/myapp/user/UserApplication.java
+++ user/src/main/java/com/myapp/user/UserApplication.java
@@ -1,9 +1,5 @@
 package com.myapp.user;
 
-import com.myapp.user.models.User;
-import com.myapp.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
