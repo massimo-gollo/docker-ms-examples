ab635342a98de73c5608422b024472c05145a7d9
massimogollo <massimo.gollo@hotmail.it>
2022-10-19 10:00:38 +0200

    add user microservice controller and repository

--- user/src/main/java/com/myapp/user/repository/UserRepository.java
+++ user/src/main/java/com/myapp/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import com.myapp.user.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends  MongoRepository<User, String>{
-    public User findByFirstName(String firstName);
-    public List<User> findByLastName(String lastName);
+    User findByFirstName(String firstName);
+    List<User> findByLastName(String lastName);
 }
 
